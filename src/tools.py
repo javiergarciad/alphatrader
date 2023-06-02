71f79354b2e0870266ea2d58c30f32a80c8113c7
javier <javier@qunattio.com>
2023-06-02 10:29:13 -0500

    Initial commit

---
+++ src/tools.py
@@ -0,0 +1,15 @@
+
+from pathlib import Path
+
+
+def get_project_root():
+    """Returns project root folder.
+    .../alphatrader/
+    """
+    return Path(__file__).parent.parent
+
+
+
+
+if __name__ == "__main__":
+    print(get_project_root())
